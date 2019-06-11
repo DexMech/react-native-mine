c0d813bcc4e75a7f0a128ac849c64859ca0075d4
alex.silva <alex.silva@icolabora.com.br>
2019-06-11 19:26:15 -0300

    agora vai

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import params from './src/params'
-
+import Field from './src/components/fields'
 
 
 export default class App extends Component {
@@ -18,6 +18,7 @@ export default class App extends Component {
     <View style={styles.container}>
       <Text style={styles.welcome}>Iniciando o Mines!</Text>
       <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
+      <Field/>
     </View>
     );
   }
